781ebf30cc03915025d55e851f79e02aad2d3dd3
blinry <mail@blinry.org>
2024-02-12 18:22:30 +0100

    Expose 4 `Terminal`s

--- examples/simple/script.js
+++ examples/simple/script.js
@@ -6,12 +6,20 @@ const shell = new LinuxBrowserShell(
         bios: "./v86/seabios.bin",
         vga_bios: "./v86/vgabios.bin",
         cdrom: "./v86/image.iso",
-        //initial_state: "./v86/initial-state.bin.zst",
+        initial_state: "./v86/initial-state.bin.zst",
     },
     document.getElementById("screen"),
-    document.getElementById("serial"),
 )
-shell.boot()
+
+let tty0 = shell.getTerminal(0)
+let tty1 = shell.getTerminal(1)
+
+tty0.attach(document.getElementById("terminal0"))
+tty1.attach(document.getElementById("terminal1"))
+
+shell.boot().then(() => {
+    console.log("Booted")
+})
 
 document.getElementById("save").onclick = async function () {
     shell.downloadState()
--- src/index.ts
+++ src/index.ts
@@ -2,134 +2,65 @@
 import V86Starter from "../v86/libv86.js"
 
 import {Mutex} from "async-mutex"
-import {Terminal} from "xterm"
+import {Terminal as XTerm} from "xterm"
 
-class LinuxBrowserShell {
-    private mutex: Mutex
-    private mutex2: Mutex
-    private emulator: any
-
-    private config: any = {
-        memory_size: 128 * 1024 * 1024,
-        vga_memory_size: 2 * 1024 * 1024,
-        disable_mouse: true,
-        autostart: true,
-        uart1: true, // enable ttyS1
-    }
-    private serialDiv?: HTMLDivElement
-
+export class Terminal {
     private prompt = "/ # "
-
-    private serialBuffer = ""
+    private mutex = new Mutex()
+    private mutex2 = new Mutex()
 
     constructor(
-        paths: {
-            wasm: string
-            bios: string
-            vga_bios: string
-            cdrom: string
-            initial_state?: string
-        },
-        screen?: HTMLDivElement,
-        serial?: HTMLDivElement,
-    ) {
-        this.mutex = new Mutex()
-        this.mutex2 = new Mutex()
+        private id: number,
+        private emulator: any,
+    ) {}
 
-        this.config["wasm_path"] = paths.wasm
-        this.config["bios"] = {url: paths.bios}
-        this.config["vga_bios"] = {url: paths.vga_bios}
-        this.config["cdrom"] = {url: paths.cdrom}
-
-        //this.config["serial_container_xtermjs"] = serial
-
-        if (screen) {
-            let screenDiv = screen
-            screenDiv.style.whiteSpace = "pre"
-            screenDiv.style.fontFamily = "monospace"
-            screenDiv.style.fontSize = "18px"
-            screenDiv.style.lineHeight = "20px"
-
-            let innerDiv = document.createElement("div")
-            let canvas = document.createElement("canvas")
-            canvas.style.display = "none"
-
-            screenDiv.appendChild(innerDiv)
-            screenDiv.appendChild(canvas)
-
-            this.config["screen_container"] = screenDiv
-        }
-
-        if (serial) {
-            this.serialDiv = serial
-        }
-
-        if (typeof paths.initial_state !== "undefined") {
-            this.config["initial_state"] = {url: paths.initial_state}
-        }
-    }
-
-    private appendToSerialDiv(text: string) {
-        /*
-        this.serialBuffer += text
-        if (this.serialBuffer.includes("\n")) {
-            if (typeof this.serialDiv !== "undefined") {
-                let currentContent = this.serialDiv.textContent
-                let maxLength = 10000
-                if (currentContent === null) {
-                    currentContent = ""
-                }
-                if (currentContent.length > maxLength) {
-                    currentContent = currentContent.slice(
-                        currentContent.length - maxLength,
-                    )
-                }
-                this.serialDiv.textContent = currentContent + this.serialBuffer
-                this.serialBuffer = ""
-            } else {
-                console.log(this.serialBuffer)
-            }
-        }
-        */
+    attach(div: HTMLElement) {
+        let term = new XTerm({
+            fontFamily: "Iosevka Nerd Font",
+        })
+        term.open(div)
+        term.onKey((key) => {
+            this.send(key.key)
+        })
+        this.emulator.add_listener(
+            `serial${this.id}-output-char`,
+            (char: string) => {
+                term.write(char)
+            },
+        )
     }
 
     async send(chars: string): Promise<void> {
-        this.emulator.serial0_send(chars)
-        this.appendToSerialDiv(chars)
+        let bytes = []
+        for (var i = 0; i < chars.length; i++) {
+            bytes.push(chars.charCodeAt(i))
+        }
+        this.emulator.serial_send_bytes(this.id, bytes)
     }
 
-    wait_for(chars: string): Promise<void> {
+    waitFor(chars: string): Promise<void> {
         return new Promise((resolve, _) => {
             let output = ""
             let listener = (char: string) => {
                 if (char !== "\r") {
                     output += char
-                    this.appendToSerialDiv(char)
                 }
                 if (output.endsWith(chars)) {
                     this.emulator.remove_listener(
-                        "serial0-output-char",
+                        `serial${this.id}-output-char`,
                         listener,
                     )
                     resolve()
                 }
             }
-            this.emulator.add_listener("serial0-output-char", listener)
+            this.emulator.add_listener(`serial${this.id}-output-char`, listener)
         })
-    }
-
-    git(command: string): Promise<string> {
-        return this.run(`git ${command}`)
-    }
-
-    async cd(path: string): Promise<void> {
-        await this.run(`cd ${path}`)
     }
 
     async run(cmd: string): Promise<string> {
         await this.mutex2.acquire()
-        let output = await this.run_unsafe(cmd)
-        let exit_code = await this.run_unsafe("echo $?")
+        let output = await this.runUnsafe(cmd)
+        let exit_code = await this.runUnsafe("echo $?")
         this.mutex2.release()
 
         if (exit_code != "0") {
@@ -138,14 +69,7 @@ class LinuxBrowserShell {
         return output
     }
 
-    async script(cmds: string[]): Promise<void> {
-        for (let cmd of cmds) {
-            await this.run(cmd)
-        }
-    }
-
-    // Run a command via the serial port (/dev/ttyS0) and return the output.
-    run_unsafe(
+    runUnsafe(
         cmd: string,
         skip_one_prompt = false,
         echo_on = true,
@@ -154,16 +78,12 @@ class LinuxBrowserShell {
             await this.mutex.acquire()
             let startTime = Date.now()
 
-            this.emulator.serial0_send(cmd + "\n")
-            if (!echo_on) {
-                this.appendToSerialDiv(cmd + "\n")
-            }
+            this.send(cmd + "\n")
 
             var output = ""
             var listener = (char: string) => {
                 if (char !== "\r") {
                     output += char
-                    this.appendToSerialDiv(char)
                 }
 
                 if (output.endsWith(this.prompt)) {
@@ -172,7 +92,7 @@ class LinuxBrowserShell {
                         return
                     }
                     this.emulator.remove_listener(
-                        "serial0-output-char",
+                        `serial${this.id}-output-char`,
                         listener,
                     )
 
@@ -191,73 +111,28 @@ class LinuxBrowserShell {
                     // Add timing information.
                     let endTime = Date.now()
                     let duration = endTime - startTime
-                    this.appendToSerialDiv(`(${duration} ms) `)
-                    if (this.serialDiv !== undefined) {
-                        this.serialDiv.scrollTop = this.serialDiv.scrollHeight
-                    }
 
                     resolve(output)
                     this.mutex.release()
                 }
             }
-            this.emulator.add_listener("serial0-output-char", listener)
+            this.emulator.add_listener(`serial${this.id}-output-char`, listener)
         })
     }
 
-    boot(): Promise<void> {
-        return new Promise((resolve, _) => {
-            // Start the this.emulator!
-            //@ts-ignore
-            this.emulator = new V86Starter(this.config)
-
-            // Wait for the this.emulator to start, then resolve the promise.
-            var interval = setInterval(async () => {
-                if (this.emulator.is_running()) {
-                    clearInterval(interval)
-
-                    // Initialize xterm.js.
-                    if (typeof this.serialDiv !== "undefined") {
-                        let term = new Terminal({
-                            fontFamily: "Iosevka Nerd Font",
-                        })
-                        term.open(document.getElementById("serial"))
-                        term.onKey((key) => {
-                            let char = key.key
-                            let bytes = []
-                            for (var i = 0; i < char.length; i++) {
-                                bytes.push(char.charCodeAt(i))
-                            }
-                            this.emulator.serial_send_bytes(1, bytes)
-                        })
-                        this.emulator.add_listener(
-                            "serial1-output-char",
-                            (char: string) => {
-                                term.write(char)
-                            },
-                        )
-                    }
-
-                    this.prompt = "WEB_SHELL_PROMPT> "
-                    await this.run_unsafe(
-                        `export PS1='${this.prompt}'`,
-                        true,
-                        true,
-                    )
-
-                    // Set terminal width so that input lines don't wrap.
-                    //await this.run_unsafe("stty cols 1000", false, true)
-
-                    // By default HOME seems to be set to "/"?
-                    //await this.run_unsafe("export HOME=/root")
-
-                    resolve()
-                }
-            }, 100)
-        })
+    async setPrompt(prompt: string): Promise<void> {
+        this.prompt = prompt
+        await this.runUnsafe(`export PS1='${this.prompt}'`, true, true)
     }
 
-    type(text: string): void {
-        this.emulator.keyboard_send_text(text)
+    async cd(path: string): Promise<void> {
+        await this.run(`cd ${path}`)
+    }
+
+    async script(cmds: string[]): Promise<void> {
+        for (let cmd of cmds) {
+            await this.run(cmd)
+        }
     }
 
     async putFile(path: string, lines: string[]): Promise<void> {
@@ -266,8 +141,79 @@ class LinuxBrowserShell {
         return
     }
 
-    setKeyboardActive(active: boolean): void {
-        this.emulator.keyboard_set_status(active)
+    //setKeyboardActive(active: boolean): void {
+    //    this.emulator.keyboard_set_status(active)
+    //}
+}
+
+export class LinuxBrowserShell {
+    private emulator: any
+    private terminals: Terminal[] = []
+
+    private config: any = {
+        memory_size: 128 * 1024 * 1024,
+        vga_memory_size: 2 * 1024 * 1024,
+        disable_mouse: true,
+
+        // enable ttyS1-ttyS3
+        uart1: true,
+        uart2: true,
+        uart3: true,
+    }
+
+    constructor(
+        paths: {
+            wasm: string
+            bios: string
+            vga_bios: string
+            cdrom: string
+            initial_state?: string
+        },
+        screen?: HTMLDivElement,
+    ) {
+        this.config["wasm_path"] = paths.wasm
+        this.config["bios"] = {url: paths.bios}
+        this.config["vga_bios"] = {url: paths.vga_bios}
+        this.config["cdrom"] = {url: paths.cdrom}
+
+        if (typeof paths.initial_state !== "undefined") {
+            this.config["initial_state"] = {url: paths.initial_state}
+        }
+
+        if (screen) {
+            console.log("Using provided screen")
+            console.log(screen)
+            let screenDiv = screen
+            screenDiv.style.whiteSpace = "pre"
+            screenDiv.style.fontFamily = "monospace"
+            screenDiv.style.fontSize = "18px"
+            screenDiv.style.lineHeight = "20px"
+
+            let innerDiv = document.createElement("div")
+            let canvas = document.createElement("canvas")
+            canvas.style.display = "none"
+
+            screenDiv.appendChild(innerDiv)
+            screenDiv.appendChild(canvas)
+
+            this.config["screen_container"] = screenDiv
+        }
+
+        this.emulator = new V86Starter(this.config)
+
+        this.terminals = [
+            new Terminal(0, this.emulator),
+            new Terminal(1, this.emulator),
+            new Terminal(2, this.emulator),
+            new Terminal(3, this.emulator),
+        ]
+    }
+
+    getTerminal(id: number): Terminal {
+        if (id < 0 || id > 3) {
+            throw new Error(`Terminal ID must be between 0 and 3, got ${id}`)
+        }
+        return this.terminals[id]
     }
 
     downloadState(filename: string = "linux-browser-shell-state.bin"): void {
@@ -280,6 +226,29 @@ class LinuxBrowserShell {
             a.click()
         })
     }
-}
 
-export default LinuxBrowserShell
+    boot(): Promise<void> {
+        return new Promise((resolve, _) => {
+            console.log("Booting")
+
+            this.emulator.add_listener("emulator-loaded", () => {
+                this.emulator.run()
+
+                // Wait for the emulator to start, then resolve the promise.
+                var interval = setInterval(async () => {
+                    if (this.emulator.is_running()) {
+                        clearInterval(interval)
+
+                        // Set terminal width so that input lines don't wrap.
+                        //await this.run_unsafe("stty cols 1000", false, true)
+
+                        // By default HOME seems to be set to "/"?
+                        //await this.runUnsafe("export HOME=/root")
+
+                        resolve()
+                    }
+                }, 100)
+            })
+        })
+    }
+}
